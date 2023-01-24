680d1618ccc6f138482fee1f755defa938a1fe70
saurabh305 <saurabhchandra305@gmail.com>
2023-01-24 19:48:22 +0530

    First commit

---
+++ EmailSender/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+using System.Linq;
+
+namespace EmailSender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SendEmail();           
+        }       
+
+        private static void SendEmail()
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential("", ""),
+                    EnableSsl = true,
+                };
+
+                //smtpClient.Send("email", "recipient", "subject", "body");
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(""),
+                    Subject = "Test Email by saurabh from c#",
+                    Body = "<h1>Hello Test mail</h1>",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add("");
+
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }       
+
+
+
+    }
+}
